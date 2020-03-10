86a742d019f54ea267bd7457cdc13fd6968733d2
Vegas <rusland174@gmail.com>
2020-03-10 16:53:45 +0500

    lession experiments

--- Chapter 6/lib.js
+++ Chapter 6/lib.js
@@ -3,14 +3,25 @@
 function User(firstName, lastName) {
 	this.firstName = firstName;
 	this.lastName = lastName;
-	this.getFullName = function () {
-		console.log(this);
-		return this.firstName + " " + this.lastName;
-	}
+	// this.getFullName = function () {
+	// 	console.log(this);
+	// 	return this.firstName + " " + this.lastName;
+	// }
 }
+
+function getFullName(prefix, suffix) {
+	return prefix
+	+ ", "
+	+ this.firstName 
+	+ " " 
+	+ this.lastName 
+	+ suffix;
+}
+
 
 var userOne = new User("Руслан", "Князев");
 var userTwo = new User("Иван", "Иванов");
 
-console.log(userOne.getFullName());
-console.log(userTwo.getFullName());
+console.log(getFullName.call(userOne, "Привет", "!"));
+console.log(getFullName.apply(userTwo, ["Привет", "!"]));
+console.log(Math.max.apply(null, [10, 5, 8, 30, 12, 64, 91]));
