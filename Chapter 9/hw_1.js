5d45d726be94addb55abd5fa65b98207c581dad9
RuslanMGN174 <rusland174@gmail.com>
2020-04-26 10:17:49 +0500

    Chapter 9 extension task

--- Chapter 9/hw_1.js
+++ Chapter 9/hw_1.js
@@ -10,31 +10,42 @@
 Если название задачи не введено, то появляется ошибка (alert или другая реализация) с просьбой заполнить поле.
 Считать значение, введенное в поле, можно при помощи document.querySelector(‘input’).value.
 При клике на любую задачу в списке она помечается как выполненная (становится зачеркнутой, 
-	тут пригодится CSS-свойство text-decoration: line-through). При повторном клике зачеркивание исчезает. 
+	тут пригодится CSS-свойство text-decoration: line-through). При повторном клике зачеркивание исчезает.
+	
+Доп.задание добавить проверку на дублирование задания.
  */
 
 
 (() => {
 	var todolist = document.querySelector("ol");
+	var listArray = [];
 
-	todolist.addEventListener("click", function (ev) {
+	todolist.addEventListener("click", ev => {
 		if (ev.target.tagName === "LI") {
 			ev.target.classList.toggle('checked');
 		}
 	}, false)
+
 
 	document
 		.getElementById("add")
 		.onclick = function () {
 			var li = document.createElement("li");
 			var someBusines = document.getElementById("in").value;
+
 			var temp = document.createTextNode(someBusines);
 			li.appendChild(temp);
 
 			if (someBusines == "") {
 				alert("Введите ваше дело!!!")
 			} else {
-				document.getElementById("list").append(li);
+
+				if (listArray.includes(someBusines)) {
+					alert("Такое дело уже есть! Введите новое дело...");
+				} else {
+					document.getElementById("list").append(li);
+					listArray.push(someBusines);
+				}
 			}
 			document.getElementById("in").value = "";
 		}
