cc030810888fb57682aae3644c86fde4c77c535b
Vegas <rusland174@gmail.com>
2020-05-07 15:58:51 +0500

    Chapter 11 add several languages

--- Chapter 11/hw_1.js
+++ Chapter 11/hw_1.js
@@ -2,6 +2,15 @@
 Создать свой маленький переводчик на основе API Яндекс.Переводчика.
  */
 
+function getLang(id) {
+	var element = document.getElementById(id);
+
+	// получаем индекс выбранного элемента
+	var selind = element.options.selectedIndex;
+
+	// получаем знчение value выбранного элемента
+	return element.options[selind].value;
+}
 
 (() => {
 	document
@@ -13,7 +22,7 @@
 
 			// Сохраняем ключ API, полученный со страницы https://tech.yandex.ru/keys/get/?service=trnsl
 			// (с примером ниже работать не будет, нужно получить и вставить свой!)
-			var API_KEY = 'some key...';
+			var API_KEY = 'trnsl.1.1.20200507T092857Z.bf6eb8b8506afd7c.67e771d60eb9906bf27ae2b9d001463b46379fd8';
 
 			// Сохраняем адрес API
 			var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
@@ -21,7 +30,7 @@
 			// Формируем полный адрес запроса:
 			url += '?key=' + API_KEY; // добавляем к запросу ключ API
 			url += '&text=' + wordForTranslate; // текст для перевода
-			url += '&lang=ru-en'; // направление перевода: с русского на английский
+			url += '&lang=' + getLang("textLangs") + "-" + getLang("transLangs"); // направление перевода
 
 			// Таким образом формируется строка вида:
 			// https://translate.yandex.net/api/v1.5/tr.json/translate?key=example_api_key&text=кролики&lang=ru-en
@@ -33,6 +42,7 @@
 					return response.json();
 				})
 				.then(function (response) {
+					console.log(response);
 
 					// Проверяем статус-код, который прислал сервер
 					// 200 — это ОК, остальные — ошибка или что-то другое
