01331510f2f291bdc4f8cc491c4697c3efb2f101
Vegas <rusland174@gmail.com>
2020-05-07 23:43:52 +0500

    Chapter 11 add count, rebild goals

--- Chapter 12/JS/hw_1.js
+++ Chapter 12/JS/hw_1.js
@@ -2,29 +2,36 @@
 
 jQuery.easing.def = "easeOutCubic";
 
-var field = document.querySelector(".field");
-var ball = document.querySelector(".ball");
+const field = document.querySelector(".field");
+const ball = document.querySelector(".ball");
 
 $('.ball').on('click', function () {
 
-	var topPlace = Math.floor(Math.random() * (field.height - ball.height + 1));
-
+	let topPlace = Math.floor(Math.random() * (field.height - ball.height + 1));
+	let goalLeftRod = (field.height / 2) - 120;
+	let goalRightRod = (field.height / 2) + 70;
+	
 	if (ball.offsetLeft == 0) {
 		$(".ball").animate({
 			"top": topPlace,
 			"left": field.width - ball.width
 		}, 1000, function () {
-			if (ball.offsetTop >= 340 && ball.offsetTop <= 440) {
+
+			if (ball.offsetTop > goalLeftRod && ball.offsetTop < goalRightRod) {
 				alert('Гооол!');
+				document.getElementById("leftCount").innerHTML++;
 			}
 		});
+
 	} else {
 		$(".ball").animate({
 			"top": topPlace,
 			"left": "0"
 		}, 1000, function () {
-			if (ball.offsetTop >= 340 && ball.offsetTop <= 440) {
+
+			if (ball.offsetTop > goalLeftRod && ball.offsetTop < goalRightRod) {
 				alert('Гооол!');
+				document.getElementById("righCount").innerHTML++;
 			}
 		});
 	}
