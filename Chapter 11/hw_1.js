d53fdbfb50c14cc1c40b18358fffa3842f6ddd87
Vegas <rusland174@gmail.com>
2020-05-08 10:29:14 +0500

    Chapter 11 ES2015 refactoring

--- Chapter 11/hw_1.js
+++ Chapter 11/hw_1.js
@@ -1,12 +1,13 @@
 /*
 Создать свой маленький переводчик на основе API Яндекс.Переводчика.
  */
+"use strict";
 
-function getLang(id) {
-	var element = document.getElementById(id);
+const getLang = (id) => {
+	const element = document.getElementById(id);
 
 	// получаем индекс выбранного элемента
-	var selind = element.options.selectedIndex;
+	const selind = element.options.selectedIndex;
 
 	// получаем знчение value выбранного элемента
 	return element.options[selind].value;
@@ -15,34 +16,31 @@ function getLang(id) {
 (() => {
 	document
 		.getElementById("btn")
-		.onclick = function (ev) {
+		.onclick = (ev) => {
 			ev.preventDefault();
 
-			var wordForTranslate = document.getElementById("in").value;
+			let wordForTranslate = document.getElementById("in").value;
 
 			// Сохраняем ключ API, полученный со страницы https://tech.yandex.ru/keys/get/?service=trnsl
 			// (с примером ниже работать не будет, нужно получить и вставить свой!)
-			var API_KEY = 'trnsl.1.1.20200507T092857Z.bf6eb8b8506afd7c.67e771d60eb9906bf27ae2b9d001463b46379fd8';
+			const API_KEY = 'trnsl.1.1.20200507T092857Z.bf6eb8b8506afd7c.67e771d60eb9906bf27ae2b9d001463b46379fd8';
 
 			// Сохраняем адрес API
-			var url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
+			let url = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 
 			// Формируем полный адрес запроса:
-			url += '?key=' + API_KEY; // добавляем к запросу ключ API
-			url += '&text=' + wordForTranslate; // текст для перевода
-			url += '&lang=' + getLang("textLangs") + "-" + getLang("transLangs"); // направление перевода
+			url += `?key=${API_KEY}`; // добавляем к запросу ключ API
+			url += `&text=${wordForTranslate}`; // текст для перевода
+			url += `&lang=${getLang("textLangs")}-${getLang("transLangs")}`; // направление перевода
 
 			// Таким образом формируется строка вида:
 			// https://translate.yandex.net/api/v1.5/tr.json/translate?key=example_api_key&text=кролики&lang=ru-en
 
-			var translate = document.querySelector('.translate');
+			let translate = document.querySelector('.translate');
 
 			window.fetch(url)
-				.then(function (response) {
-					return response.json();
-				})
-				.then(function (response) {
-					console.log(response);
+				.then((response) => response.json())
+				.then((response) => {console.log(response);
 
 					// Проверяем статус-код, который прислал сервер
 					// 200 — это ОК, остальные — ошибка или что-то другое
