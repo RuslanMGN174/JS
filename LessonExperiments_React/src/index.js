35de0400de5c05937dcfa9be65e98965162978e8
Vegas <rusland174@gmail.com>
2020-05-28 15:35:06 +0500

    React experiments v2.0 todo list app

--- LessonExperiments_React/src/index.js
+++ LessonExperiments_React/src/index.js
@@ -8,8 +8,64 @@ const HelloWorld = () => {
   return <h1>Hello world</h1>
 }
 
+// React-компонент (основанный на классе)
+class TodoApp extends React.Component {
+  constructor() {
+    super();
+
+    // Исходное состояние приложения
+    this.state = {
+      todos: [
+        { name: "Настроить webpack", checked: true },
+        { name: "Запустить webpack", checked: true },
+        { name: "Написать TodoApp", checked: false }
+      ]
+    };
+  }
+
+  toggleTodo(key) {
+    const todos = this.state.todos.map((todo, i) => {
+      if (key === i) {
+        return {
+          name: todo.name,
+          checked: !todo.checked
+        }
+      }
+
+      else {
+        return todo;
+      }
+    });
+
+    this.setState({todos});
+
+  }
+
+  render() {
+    return (
+      <ol>
+        {
+          this.state.todos.map((todo, i) => {
+            const isChecked = todo.checked ? "checked" : "";
+
+            return (
+              <li
+                key={i}
+                className={isChecked}
+                onClick={ev => { this.toggleTodo(i) }}
+              >
+                {todo.name}
+              </li>
+            )
+          })
+        }
+      </ol>
+    );
+  }
+}
+
 ReactDOM.render(
-  <HelloWorld />,
+  <TodoApp />,
   document.querySelector("#app")
 );
 
