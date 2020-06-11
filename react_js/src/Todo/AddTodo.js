9893a37bcb4d43ba5de03d8378ea2258d3f7783a
Vegas <rusland174@gmail.com>
2020-06-11 11:37:46 +0500

    remove todo added

---
+++ react_js/src/Todo/AddTodo.js
@@ -0,0 +1,3 @@
+import React, {useContext} from "react";
+import PropTypes from "prop-types";
+import Context from "../context";
--- react_js/src/Todo/TodoItem.js
+++ react_js/src/Todo/TodoItem.js
@@ -1,19 +1,50 @@
-import React from "react";
+import React, {useContext} from "react";
 import PropTypes from "prop-types";
+import Context from "../context";
 
+const styles = {
+  li: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    padding: ".5rem 1rem",
+    border: "1px solid #ccc",
+    borderRadius: "4px",
+    marginBottom: ".5rem"
+  },
+  input: {
+    marginRight: "1rem"
+  }
+}
 
-function TodoItem({ todo, index }) {
+function TodoItem({ todo, index, onChange }) {
+  const {removeTodo} = useContext(Context);
+
+  const isChecked = todo.completed ? "done" : "";
+
   return (
-    <li>
-      <strong>{index + 1}</strong>
-      {todo.title}
+    <li style={styles.li}>
+      <span className={isChecked}>
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          style={styles.input}
+          onChange={
+            () => onChange(todo.id)}
+        />
+        <strong>{index + 1}</strong>
+        &nbsp;
+        {todo.title}
+      </span>
+      <button className="rm" onClick={removeTodo.bind(null, todo.id)/*() => removeTodo(todo.id)*/}>&times;</button>
     </li>
   )
 };
 
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired,
-  index: PropTypes.number
+  index: PropTypes.number,
+  onChange: PropTypes.func.isRequired
 }
 
 export default TodoItem;
