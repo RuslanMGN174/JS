cda99327f7214e6f22fd93ad738557237349b493
Vegas <rusland174@gmail.com>
2020-06-29 09:41:28 +0500

    chapter15 final v.1.0 (add localStorage func)

--- chapter15/src/App.js
+++ chapter15/src/App.js
@@ -4,11 +4,7 @@ import CommentsList from './Comments/CommentList';
 import Context from './Context';
 
 function App() {
-  const [comments, setComments] = useState([
-    // { id: 1, author: "qwe", text: "Купить хлеб", date: new Date().toLocaleString() },
-    // { id: 2, author: "asd", text: "Купить масло", date: new Date().toLocaleString() },
-    // { id: 3, author: "zxc", text: "Купить молоко", date: new Date().toLocaleString() }
-  ]);
+  const [comments, setComments] = useLocalStorage("Comments", []);
 
   const removeComment = (id) => {
     setComments(comments.filter(comment => comment.id !== id));
@@ -16,17 +12,14 @@ function App() {
 
   const addComment = (event) => {
     event.preventDefault(event);
-    setComments(comments.concat(
+    setComments(comments.concat([
       {
         id: Date.now(),
         author: event.target.name.value,
         text: event.target.comment.value,
         date: new Date().toLocaleString()
       }
-    ))
-    localStorage.setItem("Comments", JSON.stringify(comments));
-    console.log(comments);
-    
+    ]))
   }
 
   return (
@@ -45,6 +38,42 @@ function App() {
       </div>
     </Context.Provider>
   );
+}
+
+function useLocalStorage(key, initialValue) {
+  // Стейт для хранения значений
+  // Передаем функцию начального состояния в useState, чтобы логика выполнялась только один раз
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      // Берем значения из local storage по ключу
+      const item = window.localStorage.getItem(key);
+      // Парсим получаемое значение или возвращаем initialValue
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      // Если ишибка, то возвращаем initialValue
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  // Вернуть упакованную версию setter-функции useState, которая ...
+   // ... сохраняет новое значение для localStorage.
+  const setValue = value => {
+    try {
+      // Позволить value быть функцией, так как у нас тот же API, что и для useState
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      // Сохраняем стэйт
+      setStoredValue(valueToStore);
+      // Сохраняем значения в local storage
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      // Более продвинутая реализация будет обрабатывать ошибки
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
 }
 
 export default App;
